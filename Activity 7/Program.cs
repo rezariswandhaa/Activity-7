77d869cc8c12d3ed78767c2d9528cc432fbbae99
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-12-15 16:17:45 +0700

    Add Class Queues

--- Activity 7/Program.cs
+++ Activity 7/Program.cs
@@ -2,5 +2,14 @@
 
 namespace CircularQueues_CSharp
 {
-    
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+    }
 }
