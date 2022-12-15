3e11935d00da7a385a385a003887640a2f7345f5
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-12-15 16:23:30 +0700

    Add public void insert on class Queues

--- Activity 7/Program.cs
+++ Activity 7/Program.cs
@@ -11,5 +11,28 @@ namespace CircularQueues_CSharp
             FRONT = -1;
             REAR = -1;
         }
+        public void insert(int elemen)
+        {
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = elemen;
+        }
+
     }
 }
